ede5184315b29d4132075cb782525c5df6eda71b
e570270 <e570270@sbb.ch>
2025-09-17 15:36:52 +0200

    initial commit

---
+++ BackendGibbsta/src/main/java/sbb/project/BackendGibbsta/BackendGibbstaApplication.java
@@ -0,0 +1,13 @@
+package sbb.project.BackendGibbsta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGibbstaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGibbstaApplication.class, args);
+	}
+
+}
---
+++ BackendGibbsta/src/test/java/sbb/project/BackendGibbsta/BackendGibbstaApplicationTests.java
@@ -0,0 +1,13 @@
+package sbb.project.BackendGibbsta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGibbstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
